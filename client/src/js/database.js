9308c4dfdb2a0b4f8a7ba02f8649730ac43702de
joostyn <justyn.helgeson@gmail.com>
2022-08-22 17:44:18 -0500

    Updated webpack and database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jate_db = await openDB('jate', 1);
+  const tx = jate_db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, text: content})
+  const result = await request;
+  console.log('data updated in jate database', result);
+};
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jate_db = await openDB('jate', 1);
+  const tx = jate_db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('data saved to jate database', result);
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // TODO: Add and configure workbox plugins for a service worker and manifest file.
+      new HtmlWebpackPlugin({
+        title: 'JATE',
+        filename: 'index.html',
+        template: path.join(__dirname, 'src/index.html'),
+        inject: 'body'
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Setting Up a PWA Text Editor',
+        background_color: '#555555',
+        start_url: './',
+        publicPath: './',
+        inject: true,
+        theme_color: '#555555',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            ios: true
+          },
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
       rules: [
-        
+        // TODO: Add CSS loaders and babel to webpack.
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
